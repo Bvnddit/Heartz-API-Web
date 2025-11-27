c646db27f625bf228d2b5c85800a22aaf3925385
bvnddit <alangadiel11@gmail.com>
2025-11-27 20:16:12 -0300

    Documentación de DTOs para referenciar

--- src/main/java/com/heartz/heartz_api/dto/DetalleVentaDTO.java
+++ src/main/java/com/heartz/heartz_api/dto/DetalleVentaDTO.java
@@ -1,8 +1,10 @@
 package com.heartz.heartz_api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "DTO para crear un nuevo detalle de venta")
 public class DetalleVentaDTO {
     private Integer idVinilo;
     private Integer cantidad;
--- src/main/java/com/heartz/heartz_api/dto/VentaDTO.java
+++ src/main/java/com/heartz/heartz_api/dto/VentaDTO.java
@@ -3,8 +3,12 @@ package com.heartz.heartz_api.dto;
 import lombok.Data;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Data
+@Schema(description = "DTO para crear una nueva venta")
 public class VentaDTO {
+    @Schema(description = "RUT del usuario que realiza la venta")
     private String rutUsuario;
     private Integer total;
     private List<DetalleVentaDTO> detalles;
